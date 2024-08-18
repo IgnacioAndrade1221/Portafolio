a414150ace62330e0bc4b146e6b582b3a0389729
IGNACIO ANDRADE <ignacio.andrade1221@gmail.com>
2024-08-18 19:42:57 -0400

    arreglos

--- menu.js
+++ menu.js
@@ -1,5 +1,11 @@
-const nav=document.querySelector('.interfaz');
+const nav = document.querySelector('.interfaz');
 
-window.addEventListener('scroll',function(){
-    nav.classList.toggle('activate', window.scrollY > 0);
+window.addEventListener('scroll', function() {
+    // Calcula la opacidad basada en el scroll
+    const scrollAmount = window.scrollY;
+    const maxScroll = document.body.scrollHeight - window.innerHeight;
+    const opacity = 1 - (scrollAmount / maxScroll);
+
+    // Aplica la opacidad al nav
+    nav.style.opacity = Math.max(opacity, 0.50); // Asegura que la opacidad no sea menor a 0.3
 });
